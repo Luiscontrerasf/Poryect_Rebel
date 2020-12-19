3196ae020eb47b6592f201f39337cabf396d0632
Luis Contreras <luis.e.contreras1607@gmail.com>
2020-12-19 20:00:44 -0300

    aliminar

--- applications/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import  TemplateView, ListView
-
-# Create your views here.
-class index(TemplateView):
-    template_name='home/index.html'
-
-
-    
