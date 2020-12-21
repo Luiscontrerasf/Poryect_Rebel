d84881b93037dda7fdb32e3301997f1fdecf74fe
gabo013 <gab.lazcano@alumnos.duoc.cl>
2020-12-21 00:45:53 -0300

    eliminar todo

--- applications/servicio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-def testeo2(self):
-    print('================hola==============')
-urlpatterns = [
-    path('servicio/',testeo2),
-]
