f9f3423b23cb5bb45de51ef2effc16f14ffd1382
gabo013 <gab.lazcano@alumnos.duoc.cl>
2020-12-21 23:36:20 -0300

    cambios a la url

--- rebel_app/urls.py
+++ rebel_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('Tortas.html', views.torta, name="torta"),
+    path('Tortas/', views.torta, name="torta"),
     path('Dulces.html', views.dulce, name="dulce"),
     path('Salados.html', views.salado, name="salado"),
     path('contacto/', views.contacto, name="contacto"),
