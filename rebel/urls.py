20bcc148310e98068fc4b3b2650bd2fe53f5588c
Luis Contreras <luis.e.contreras1607@gmail.com>
2020-12-22 20:17:19 -0300

    arreglando detalles

--- rebel/urls.py
+++ rebel/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
     path('', include('rebel_app.urls')),
     path('', include('pwa.urls')),    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns() 
